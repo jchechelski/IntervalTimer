67ba2700086b00e8ab22ba070e7093f33e435ecf
Jarek <chechelskiwsei@gmail.com>
2017-03-15 20:42:18 +0100

    dsa

--- www/js/script.js
+++ www/js/script.js
@@ -36,54 +36,69 @@ function onDeviceReady() {
     rundaHTML.innerHTML ='Runda ' + runda + '/' + przejscia;
     time.innerHTML = timeSet;
     
-    
+    var checkOnOff = false;
     function go() {
-        navigator.notification.beep(1);
-        play.disabled = true;
-        
-        timer = setInterval(function() {
-            --timeSet;
-            time.innerHTML = timeSet;
-            if(timeSet===0 || timeSet <= 0) {
-                clearInterval(timer);
-                ++runda;
-                breakTimer = setInterval(function() {
-                    if(runda > przejscia) {
-                        clearInterval(timer);
-                        clearInterval(breakTime);
-                        navigator.notification.alert('KONIEC!');
-                    }
-                    --przerwa;
-                    przerwaHTML.innerHTML = 'przerwa: ' + przerwa;
-                    rundaHTML.innerHTML ='Runda ' + runda + '/' + przejscia;
-                    if(przerwa == 0 || przerwa <= 0) {
-                        clearInterval(breakTimer);
-                        timeSet = czas;
-                        przerwa = document.getElementById('przerwa').value; 
+        if(checkOnOff == false) {
+            navigator.notification.beep(1);
+
+            play.style.background = '#ed1616';
+
+            timer = setInterval(function() {
+                --timeSet;
+                time.innerHTML = timeSet;
+                if(timeSet===0 || timeSet <= 0) {
+                    ++runda;
+                    clearInterval(timer);
+                    breakTimer = setInterval(function() {
+                        if(runda > przejscia) {
+                            clearInterval(timer);
+                            clearInterval(breakTime);
+                        }
+                        --przerwa;
                         przerwaHTML.innerHTML = 'przerwa: ' + przerwa;
-                        time.innerHTML = timeSet;
-                        go();
-                    }
-                },speed);
-            }
-        },speed);
+                        rundaHTML.innerHTML ='Runda ' + runda + '/' + przejscia;
+                        if(przerwa == 0 || przerwa <= 0) {
+                            
+                            clearInterval(breakTimer);
+                            timeSet = czas;
+                            przerwa = document.getElementById('przerwa').value; 
+                            przerwaHTML.innerHTML = 'przerwa: ' + przerwa;
+                            time.innerHTML = timeSet;
+                            checkOnOff = false;
+                            go();
+                        }
+                    },speed);
+                }
+            },speed);
+        }
+
         
-        
+       checkOnOff = true; 
     }
     
     function pauseF() {
+        var tempBreak = przerwa;
+        var tempRunda = runda;
+        runda = --tempRunda;
+        przerwa = tempBreak;
+        var tempTime = timeSet;
+        timeSet = ++tempTime;
+        checkOnOff = false;
+        play.style.background = '#0b0b0b';
         clearInterval(timer); 
         clearInterval(breakTimer);
-        play.disabled = false;
     }
     
     function restartF() {
         clearInterval(timer);
+        clearInterval(breakTimer);
         timeSet = czas;
         time.innerHTML = timeSet;
         runda = 1;
         przerwaHTML.innerHTML = 'przerwa: ' + przerwa;
         rundaHTML.innerHTML ='Runda ' + runda + '/' + przejscia;
+        play.style.background = '#0b0b0b';
+        checkOnOff = false;
     }
     
     play.addEventListener('click', go);
