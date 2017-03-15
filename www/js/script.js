ef158e63b4f9b4948c30c2b6964d4e917dd18e8d
Jarek <chechelskiwsei@gmail.com>
2017-03-15 19:21:17 +0100

    work

--- www/js/script.js
+++ www/js/script.js
@@ -23,37 +23,67 @@ function onDeviceReady() {
         przejscia = document.getElementById('przejscia').value,
         zapisz = document.getElementById('zapisz');
     
+    var przerwaHTML = document.querySelector('.break'),
+        rundaHTML = document.querySelector('.set');
+    
+    
+    
     var timeSet = parseInt(czas),
+        runda = 1,
         speed = 1000;
     
+    przerwaHTML.innerHTML = 'przerwa: ' + przerwa;
+    rundaHTML.innerHTML ='Runda ' + runda + '/' + przejscia;
     time.innerHTML = timeSet;
     
     
     function go() {
         navigator.notification.beep(1);
-        //play.disable = true;
+        play.disabled = true;
+        
         timer = setInterval(function() {
             --timeSet;
             time.innerHTML = timeSet;
-            //if(timeSet < 4) {
-            //  navigator.notification.beep(1);
-            //}
             if(timeSet===0 || timeSet <= 0) {
                 clearInterval(timer);
-                timeSet = czas;
-                time.innerHTML = timeSet;
+                ++runda;
+                breakTimer = setInterval(function() {
+                    if(runda > przejscia) {
+                        clearInterval(timer);
+                        clearInterval(breakTime);
+                        navigator.notification.alert('KONIEC!');
+                    }
+                    --przerwa;
+                    przerwaHTML.innerHTML = 'przerwa: ' + przerwa;
+                    rundaHTML.innerHTML ='Runda ' + runda + '/' + przejscia;
+                    if(przerwa == 0 || przerwa <= 0) {
+                        clearInterval(breakTimer);
+                        timeSet = czas;
+                        przerwa = document.getElementById('przerwa').value; 
+                        przerwaHTML.innerHTML = 'przerwa: ' + przerwa;
+                        time.innerHTML = timeSet;
+                        go();
+                    }
+                },speed);
             }
         },speed);
+        
+        
     }
     
     function pauseF() {
-        clearInterval(timer);        
+        clearInterval(timer); 
+        clearInterval(breakTimer);
+        play.disabled = false;
     }
     
     function restartF() {
         clearInterval(timer);
         timeSet = czas;
         time.innerHTML = timeSet;
+        runda = 1;
+        przerwaHTML.innerHTML = 'przerwa: ' + przerwa;
+        rundaHTML.innerHTML ='Runda ' + runda + '/' + przejscia;
     }
     
     play.addEventListener('click', go);
@@ -63,26 +93,21 @@ function onDeviceReady() {
     
     zapisz.addEventListener('click', function() {
         
-        czas = document.getElementById('czas').value
+        przerwa = document.getElementById('przerwa').value;
+        czas = document.getElementById('czas').value;
+        przejscia = document.getElementById('przejscia').value;
+        
         timeSet = parseInt(czas);
         //navigator.notification.alert(timeSet);
         time.innerHTML = timeSet;
+        przerwaHTML.innerHTML = 'przerwa: ' + przerwa;
+        rundaHTML.innerHTML ='Runda ' + runda + '/' + przejscia;
         
-        // start click
-        eventFire(document.getElementById('start'), 'click');
+
     });
 
     
-    // click simulation
-        function eventFire(el, etype){
-            if (el.fireEvent) {
-                el.fireEvent('on' + etype);
-            } else {
-            var evObj = document.createEvent('Events');
-            evObj.initEvent(etype, true, false);
-            el.dispatchEvent(evObj);
-      }
-}
+
     
 }
 
